14622de7456383acadc5861ec6023ad876f7e08f
bams <bbg2021@gmail.com>
2020-11-06 13:13:34 +0800

    starter typescript with webpack dev and production

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-// Code goes here!
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event){
+    event.preventDefault()
+    const enteredAddress = addressInput.value;
+    console.log(enteredAddress)
+}
+
+form.addEventListener("submit", searchAddressHandler)
